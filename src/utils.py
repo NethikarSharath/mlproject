2d49fe3e4f3f64903f9ad43e06703bd2e0049f7c
Nethikar Sharath <nethikarsharath@gmail.com>
2026-05-15 20:40:12 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -69,3 +69,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models,params):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj: 
+            # Open the file in binary read mode and load the object using dill.
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)    
